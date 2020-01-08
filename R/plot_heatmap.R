bc4a9b5600a1fa23de3dbb7599d57aab10938f9a
rscherrer <raphael.scherrer@evobio.eu>
2020-01-08 16:33:50 +0100

    custom colors for heatmap

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -16,6 +16,7 @@
 #' @param aggregate Method used to aggregate replicate simulations. Either of "average", "variance", "quantile" or "number" (the number of replicates above a certain threshold of the summary statistic).
 #' @param prob The quantile used to aggregate replicates. Used only if the aggregation method is "quantile".
 #' @param threshold The threshold used if the aggregation method is "number".
+#' @param colors Optional low and high ends of the color gradient
 #' @param splitvar Facet splitting variable
 #' @param splitvar2 Second facet splitting variable
 #'
@@ -36,6 +37,7 @@ plot_heatmap <- function(
   aggregate = "average",
   prob = 0.95,
   threshold = 0.9,
+  colors = NULL,
   splitvar = NULL,
   splitvar2 = NULL
 ) {
@@ -65,6 +67,8 @@ plot_heatmap <- function(
   # Make the heatmap
   p <- ggplot(data = dred, aes(x = get(xname), y = get(yname), fill = Z)) +
     geom_tile()
+
+  if (!is.null(colors)) p <- p + scale_color_gradient(low = colors[1], high = colors[2])
 
   if (!is.null(labs)) p <- p + xlab(labs[1]) + ylab(labs[2])
 
