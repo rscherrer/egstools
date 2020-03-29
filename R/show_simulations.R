90977be2ca98e50553847d7c80319b7bdf4a4dd4
rscherrer <raphael.scherrer@evobio.eu>
2020-03-29 21:53:51 +0200

    stuff

---
+++ R/show_simulations.R
@@ -0,0 +1,6 @@
+rm(list = ls())
+
+setwd("/media/raphael/bigass/simulations/EGS/additive/300 genes")
+
+list.files()
+
--- results/project2/project2.R
+++ results/project2/project2.R
@@ -10,125 +10,151 @@
 rm(list = ls())
 
 library(dplyr)
+library(tidyr)
 library(ggplot2)
+library(gridExtra)
 
 # Read the data produced by the Mathematica script
-data <- read.csv("../adaptive_dynamics.csv", header = FALSE)
+data <- read.csv("/home/raphael/adaptive_dynamics1.csv", header = FALSE)
 colnames(data) <- c("h", "s", "alpha", "m", "x0", "N1", "N2", "equilibrium", "invasibility", "convergence")
 
+data_raw <- data
+
+sapply(data, function(x) any(is.na(x)))
+
+x0 <- -1
+
+smr <- data %>%
+  filter(N1 >= 0, N2 >= 0, equilibrium <= 1, equilibrium >= -1) %>%
+  distinct() %>%
+  group_by(h, s, alpha, m) %>%
+  mutate(
+    isconv = convergence < 0,
+    isinv = invasibility > 0,
+    isbp = isconv & isinv,
+    isess = isconv & !isinv,
+    isrep = !isconv,
+    distance = abs(equilibrium - x0)
+  ) %>%
+  summarize(
+    closest = min(which(distance == min(distance))),
+    isclosestconv = isconv[closest],
+    isclosestbp = ifelse(isclosestconv, isinv[closest], FALSE),
+    isanyopposite = any(sapply(equilibrium, function(x) sign(x - x0) != sign(equilibrium[closest] - x0))),
+    distopposite = ifelse(isanyopposite, min(sapply(equilibrium, function(x) ifelse(sign(x - x0) != sign(equilibrium[closest] - x0), abs(x - x0), NA)), na.rm = TRUE), NA),
+    opposite = ifelse(isanyopposite, min(which(sapply(equilibrium, function(x) abs(x - x0) == distopposite & sign(x - x0) != sign(equilibrium[closest] - x0)))), NA),
+    isoppositeconv = isconv[opposite],
+    isoppositebp = ifelse(isoppositeconv, isinv[opposite], FALSE),
+    bp = isclosestbp | !isclosestconv & isanyopposite & isoppositebp,
+    isanyinv = any(isinv > 0)
+  )
+
+ggplot(smr, aes(x = h, y = s, fill = isclosestbp)) + geom_tile() + facet_grid(alpha ~ m)
+
 # Remove duplicate equilibrium points
-data <- data[,colnames(data) != "x0"]
-resolution <- 4
+data <- data[, colnames(data) != "x0"]
+resolution <- 10
 data[, sapply(data, is.numeric)] <- round(data[, sapply(data, is.numeric)], resolution)
 data <- data %>% distinct()
 
-# Keep only those this viable population
+# Keep only those with viable population
 data <- data %>% filter(N1 > 0, N2 > 0)
-
-smr <- data %>%
-  group_by(h, s, alpha, m) %>%
-  summarize(isbranching = any(invasibility > 0.001 & convergence < -0.001))
-
-ggplot(smr, aes(x = h, y = s, fill = isbranching)) +
-  geom_tile() +
-  facet_grid(m ~ alpha)
-
-# Deduce the nature of each equilibrium
-data$stability <- "Repeller"
-data$stability[data$convergence < 0 & data$invasibility < 0] <- "ESS"
-data$stability[data$convergence > 0 & data$invasibility < 0] <- "Garden of Eden"
-data$stability[data$convergence < 0 & data$invasibility > 0] <- "Branching point"
-
-
-
-# Plot the equilibria across parameter space
-p <- ggplot(data %>% filter(h %in% c(0, 0.25, 0.5, 0.75, 1), m == 0.01, alpha == 0), aes(x = s, y = equilibrium, color = stability))
-p <- p + geom_point()
-p <- p + theme_bw()
-p <- p + ylab("Equilibrium trait value")
-p <- p + xlab("Selection coefficient")
-p <- p + labs(color = "")
-p <- p + scale_color_manual(values = c("lightgreen", "gray30", "gray50", "gray80"))
-facets <- paste("h =", levels(factor(data$h)))
-names(facets) <- levels(factor(data$h))
-p <- p + facet_wrap(~ h, labeller = labeller(h = facets))
-p
-
-# ggsave("results/project2/equilibria.png", p, width = 5, height = 3, dpi = 300)
 
 head(data)
 
-data %>% group_by(h, s, alpha, x0)
+length(which(is.na(data$N1)))
+nrow(data)
 
-# Predict evolutionary outcomes for given starting conditions
-xstart <- -1
-lambda <- function(above, below, convabove, convbelow) {
-  pair <- c(convabove, convbelow)
-  if (any(pair == 0)) return (c(above, below)[which(pair != 0)])
-  return (c(above, below)[which(pair == min(pair))])
-}
-d <- data %>%
-  mutate(
-    diff = xstart - equilibrium,
-    isabove = diff < 0,
-    isbelow = diff >= 0,
-    distance = abs(diff)
-  ) %>%
-  group_by(s, h) %>%
+smr <- data %>%
+  group_by(h, s, alpha, m) %>%
   summarize(
-    above = ifelse(any(isabove), equilibrium[distance == min(distance[isabove])], Inf),
-    below = ifelse(any(isbelow), equilibrium[distance == min(distance[isbelow])], -Inf),
-    convabove = ifelse(any(equilibrium == above), as.numeric(convergence[equilibrium == above] < 0), 2),
-    convbelow = ifelse(any(equilibrium == below), as.numeric(convergence[equilibrium == below] < 0), 2),
-    equil = lambda(above, below, convabove, convbelow),
-    outcome = ifelse(equil %in% c(-Inf, Inf), "Runaway", stability[which(equilibrium == equil)[1]])
+    anyconv = any(convergence < 0),
+    anybp = any(invasibility > 0),
+    anyess = any(invasibility < 0),
+    meaninv = mean(invasibility),
+    nconv = length(which(convergence < 0)),
+    anybpoutside = any(convergence < 0 & invasibility > 0 & equilibrium >= 0.01 & equilibrium <= -0.001),
+    nbp = length(which(convergence < 0 & invasibility > 0))
   )
 
-# Customize outcome labels
-bound <- 0.2
-d$outcome[d$outcome == "ESS"] <- "Specialist"
-d$outcome[d$outcome == "Specialist" & d$equil > -bound & d$equil < bound] <- "Generalist"
-d$outcome <- gsub("Branching point", "Branching", d$outcome)
+ggplot(smr, aes(x = h, y = s, fill = anybp)) + geom_tile() + facet_grid(alpha ~ m)
 
-# Plot across parameter space
-p <- ggplot(d, aes(x = hsymmetry, y = ecosel, fill = outcome))
-p <- p + geom_tile()
-p <- p + theme_bw()
-p <- p + xlab("Habitat symmmetry")
-p <- p + ylab("Selection coefficient")
-p <- p + labs(fill = "")
-p <- p + scale_fill_manual(values = c("lightgreen", "gray70", "gray50", "gray10"))
-p
+head(smr)
 
-# ggsave("results/project2/outcomes_highres.png", p, width = 6, height = 5)
+data <- rbind(data, do.call(rbind, lapply(c(-Inf, Inf), function(x) {
+  cbind(
+    data %>% expand(h, s, alpha, m),
+    data.frame(
+      N1 = NA,
+      N2 = NA,
+      equilibrium = x,
+      invasibility = NA,
+      convergence = -1
+    )
+  )
+})))
 
-#### With two patches
+xstart <- -1.1
 
-data <- read.csv("results/project2/twopatches.csv", header = FALSE)
-colnames(data) <- c("hsymmetry", "ecosel", "x1", "conv1", "inv1", "x2", "conv2", "inv2")
+smr <- data %>%
+  group_by(h, s, alpha, m) %>%
+  mutate(
+    isbelow = equilibrium < xstart,
+    isabove = equilibrium > xstart
+  ) %>%
+  summarize(
+    mindist.a = min(abs(xstart - equilibrium[isabove])),
+    closest.a = min(which(abs(xstart - equilibrium) == mindist.a & isabove)),
+    equil.a = equilibrium[closest.a],
+    conv.a = convergence[closest.a],
+    mindist.b = min(abs(xstart - equilibrium[isbelow])),
+    closest.b = min(which(abs(xstart - equilibrium) == mindist.b & isbelow)),
+    equil.b = equilibrium[closest.b],
+    conv.b = convergence[closest.b],
+    closest = min(which(c(mindist.a, mindist.b) == min(mindist.a, mindist.b))),
+    equil = ifelse(c(conv.a, conv.b)[closest] < 0, c(equil.a, equil.b)[closest], c(equil.a, equil.b)[-closest]),
+    N1 = N1[min(which(equilibrium == equil))],
+    N2 = N2[min(which(equilibrium == equil))],
+    inv = invasibility[min(which(equilibrium == equil))],
+    conv = convergence[min(which(equilibrium == equil))],
+    anyconv = any(conv < 0),
+    nconv = length(which(conv < 0))
+  ) %>%
+  select(h, s, alpha, m, N1, N2, equil, inv, conv, anyconv, nconv)
 
-# Here we used two different starting points for the equilibrium search
+# How many equilibria?
+ggplot(smr, aes(x = h, y = s, fill = nconv)) +
+  geom_tile() +
+  facet_grid(m ~ alpha)
 
-# If they are not the same, is at least one convergent stable?
-noproblem <- with(data, abs(x1 - x2) < 0.001 | abs(x1 - x2) >= 0.001 & (conv1 < 0 | conv2 < 0))
-data <- data[noproblem, ]
+data_raw %>% filter(h == 0.5, s == 1, alpha == 0, m == 0.001)
 
-data <- cbind(data, t(mapply(function(x1, x2, conv1, conv2, inv1, inv2) {
+#------------------
 
-  if (abs(x1 - x2) < 0.001) x <- x1
-  else if (conv1 > 0 & conv2 > 0) x <- NA
-  else if (conv1 < 0 & conv2 < 0) x <- x1
-  else if (conv1 < 0) x <- x1
-  else x <- x2
+pip <- read.csv("../pip.csv", header = FALSE)
+pip <- data.frame(sapply(pip, round, 6))
+colnames(pip) <- c("y", "x", "N1", "N2", "lambda")
 
-  if (x == x1) inv <- inv1
-  else if (x == x2) inv <- inv2
-  else inv <- NA
+head(pip)
 
-  return(c(x = x, inv = inv))
+p1 <- ggplot(pip, aes(x = x, y = y, fill = lambda > 1)) + geom_tile() + scale_fill_manual(values = c("black", "grey70"))
 
-}, data$x1, data$x2, data$conv1, data$conv2, data$inv1, data$inv2)))
+diagonal <- pip %>%
+  group_by(x) %>%
+  summarize(
+    below = ifelse(any(y == x - 0.01), lambda[y == x - 0.01], NA),
+    at = lambda[y == x],
+    above = ifelse(any(y == x + 0.01), lambda[y == x + 0.01], NA)
+  ) %>%
+  mutate(
+    diff = above - below
+  )
 
-p <- ggplot(data, aes(x = hsymmetry, y = ecosel, fill = inv > 0))
-p <- p + geom_tile()
-p
+p2 <- ggplot(diagonal, aes(x = x, y = diff)) + geom_line() + xlim(c(-0.1, 0.1))
+
+p3 <- ggplot(pip %>% filter(x == -0.5), aes(x = y - x, y = lambda)) + geom_line()
+p4 <- ggplot(pip %>% filter(x == 0), aes(x = y - x, y = lambda)) + geom_line()
+
+grid.arrange(p1, p2, p3, p4)
+
+#------------------
